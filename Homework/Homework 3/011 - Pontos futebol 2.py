f11b4b47d4a4443158c6ceb7058c022ed50ee366
Odaviespecifico <jak_singer@hotmail.com>
2025-03-15 17:01:50 -0300

    Add 011

---
+++ Homework/Homework 3/011 - Pontos futebol 2.py
@@ -0,0 +1,6 @@
+#Consulte a questão 003 para a resposta. é a mesma pergunta
+vitórias = int(input("Quantas vitórias o time teve? "))
+empates = int(input("Quantos empates o time teve? "))
+# 1.5) Calcular o número de pontos (Nesse código vou fazer dentro do print)
+# 2) mostrar o número de pontos
+print(f'O time ganhou {vitórias} vezes e empatou {empates} vezes. Os pontos são {vitórias * 3 + empates}')

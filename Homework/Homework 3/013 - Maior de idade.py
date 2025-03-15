ca0719d7aa3eff72382eb627acbdebae103df093
Odaviespecifico <jak_singer@hotmail.com>
2025-03-15 17:23:46 -0300

    add 014

--- Homework/Homework 3/013 - Maior de idade.py
+++ Homework/Homework 3/013 - Maior de idade.py
@@ -1,1 +1,16 @@
-print("Cavalinho")
+#Escreva um programa que lê a idade de um usuario e em seguida diz se o usuário é ou não maior de idade.
+
+#passos
+#1) ler idade
+#2) se a idade >= 18 dizer que é maior. senão dizer que não
+
+#1)
+idade = int(input("Quantos anos você tem?"))
+
+#2
+if idade >= 200: #Isso é uma piada. Não é necessário
+    print("Ora ora temos um imortal aqui kk")
+elif idade >= 18: #Maior ou = se diz >=
+    print(f'Você tem {idade} anos e é maior de idade')
+else:
+    print("Você não é maior de idade")

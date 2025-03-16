ffee329fc4c304b826b2f9a699248c290f730099
Odaviespecifico <jak_singer@hotmail.com>
2025-03-16 01:47:05 -0300

    Add csv no 017

--- teste.py
+++ teste.py
@@ -1,18 +1,7 @@
-ordenada = False
-numeros = []
-Qnum = int(input("Quantos números você quer adicionar? "))
-for i in range(Qnum):
-    numeros.append(int(input("Digite o número: ")))
+import csv
+notas = [5.95, 9.72, 6.8, 5.4, 6.92, 6.85, 9.97, 6.42, 9.51, 7.9, 1.86, 8.9, 8.31, 9.47, 5.51, 3.87, 6.99, 4.35, 5.52, 1.58, 6.43, 7.21, 1.65, 6.43, 9.18, 6.75, 2.59, 9.08, 1.94, 3.7, 5.08, 8.27, 7.3, 5.06, 6.41, 8.23, 6.47, 9.93, 7.04, 9.51, 5.94, 8.16, 3.72, 1.2, 8.36, 9.75, 7.3, 8.22, 2.96, 4.36, 7.33]
 
-Qnum = len(numeros)
-while ordenada == False: #Isso é uma forma bem rudimentar do bubble sort
-    emordem = numeros.copy() #Cria uma cópia do número
-    for i in range(0, len(numeros)): #Vai subsittuindo um número de cada vez
-        try:
-            if numeros[i] < numeros[i+1]: #Verifica se o número é menor do que o próximo da lista (Se não for não precisa mudar)
-                numeros[i+1],numeros[i] = numeros[i],numeros[i+1] #Inverte os números
-                print(numeros)
-        except IndexError:
-            if emordem == numeros: #Verifica se está ordenada. Se o ultimo passo deu o mesmo resultado 2 vezes signfica que está ordenada
-                print(f"A lista ordenada é {numeros}")
-                ordenada = True
+with open("notas.csv","w") as TextodeNotas:
+    for i in range(1,len(notas)):
+        csv.writer(TextodeNotas,lineterminator="\n").writerow((f'Nota do aluno {i}',notas[i]))        
+    

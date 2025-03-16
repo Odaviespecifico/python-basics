e48fe02333877a26cc14d8c396cdccfbfb760e7c
Odaviespecifico <jak_singer@hotmail.com>
2025-03-16 00:39:59 -0300

    finalizado o bubble sort em teste

--- teste.py
+++ teste.py
@@ -6,8 +6,8 @@ numeros = []
 numeros = [3,5,6,7,3,4,5,2,1,6,3,7]
 Qnum = len(numeros)
 while ordenada == False: #Isso é uma forma bem rudimentar do bubble sort
+    emordem = numeros
     for i in range(0, len(numeros)+1): #Vai subsittuindo um número de cada vez
-        emordem = numeros
         try:
             if numeros[i] < numeros[i+1]:
                 temp = numeros[i+1]
@@ -15,11 +15,11 @@ while ordenada == False: #Isso é uma forma bem rudimentar do bubble sort
                 numeros[i] = temp
                 print(numeros)
         except IndexError:
-            print("Lista finalizada")
+            print("Iteração finalizada")
+            if emordem == numeros: #Verifica se está ordenada. Se o ultimo passo deu o mesmo resultado 2 vezes signfica que está ordenada
+                print("Está ordenada")
+                print(f"A lista ordenada é {numeros}")
+                break
+            else:
+                print("Não está ordenada")
             break
-    if emordem == numeros: #Verifica se está ordenada. Se o ultimo passo deu o mesmo resultado 2 vezes signfica que está ordenada
-        print("Está ordenada")
-        print(f"A lista ordenada é {numeros}")
-        break
-    else:
-        print("Não está ordenada")

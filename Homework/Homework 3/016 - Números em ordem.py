1e980dc6ac29ac66674c1f1035dba0014b8af4ab
Odaviespecifico <jak_singer@hotmail.com>
2025-03-15 23:11:03 -0300

    add 016 (Incompleto)

---
+++ Homework/Homework 3/016 - Números em ordem.py
@@ -0,0 +1,41 @@
+#Escreva um programa que lê três número s inteiros e em seguida imprime os números em ordem crescente
+#OBS: O python tem uma função de ordenar listas. Caso você utilizasse ela o código seria bem menos porém como estamos focando em aprender os fundamentos de python vou fazer sem utilizar essa função. Em um exemplo real o ideal seria apenas usar a função. Para isso vamos utilizar um método bem básico de ordenamento de lista. O jeito que esse código foi feito também não é muito eficiente. Se fossemos usar um for loop conseguiriamos fazer ele funcionar de forma mais escalável. Vou demonstrar como isso funcionaria mais abaixo
+#Passos:
+#01) Pegar os números e colocar em uma lista
+#2) Iniciar um loop que verifica se a lista está ordenada. Numa lista ordenada um número vai ser sempre maior que o proximo. 
+#3) Verificar se os números estão ordenados. Caso não estejam, alterar a ordem
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o segundo número: "))
+numeros = [num1, num2, num3]
+
+while not(num1 >= num2 >= num3): #Enquanto a lista não estiver em ordem
+    if num1 <= num2:
+        temporário = num1
+        num1 = num2
+        num2 = temporário
+    if num2 <= num3:
+        temporário = num2
+        num3 = temporário
+        num3 = num2
+else: #Usar else depois de while loop mostra uma mensagem ao final do loop
+    print(f"Os números ordenados são {num1}, {num2}, {num3}")
+    
+    
+#Se fossemos fazer um algorítimo de ordenar lista fariamos:
+#Descobrir se a lista está ordenada
+ordenada = False
+while ordenada == False: #Isso é uma forma bem rudimentar do bubble sort
+    emordem = numeros
+    for i in range(len(numeros)): #Vai subsittuindo um número de cada vez
+        if numeros[i-1] < numeros[i]:
+            temp = numeros[i-1]
+            numeros[i-1] = numeros[i]
+            numeros[i] = temp
+    if emordem == numeros: #Verifica se está ordenada. Se o ultimo passo deu o mesmo resultado 2 vezes signfica que está ordenada
+        print("Está ordenada")
+        print(f"A lista ordenada é {numeros}")
+        break
+    else:
+        print("Não está ordenada")

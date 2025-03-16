5016df80b03eab1feefd2a0fd3f061d0c097f95b
Odaviespecifico <jak_singer@hotmail.com>
2025-03-16 00:41:27 -0300

    add 016

--- Homework/Homework 3/016 - Números em ordem.py
+++ Homework/Homework 3/016 - Números em ordem.py
@@ -23,26 +23,22 @@ else: #Usar else depois de while loop mostra uma mensagem ao final do loop
     print(f"Os números ordenados são {num1}, {num2}, {num3}")
     
     
-#Se fossemos fazer um algorítimo de ordenar lista fariamos:
-#Descobrir se a lista está ordenada
+#Se fossemos fazer um algorítimo de ordenar lista fariamos: (Essa é uma aplicação de bubble sort)
 ordenada = False
 numeros = []
 Qnum = int(input("Quantos números você quer adicionar? "))
 for i in range(Qnum):
     numeros.append(int(input("Digite o número: ")))
+
+Qnum = len(numeros)
 while ordenada == False: #Isso é uma forma bem rudimentar do bubble sort
-    emordem = numeros
-    for i in range(1, len(numeros)+1): #Vai subsittuindo um número de cada vez
+    emordem = numeros.copy() #Cria uma cópia do número
+    for i in range(0, len(numeros)): #Vai subsittuindo um número de cada vez
         try:
-            if numeros[i] < numeros[i+1]:
-                temp = numeros[i-1]
-                numeros[i-1] = numeros[i]
-                numeros[i] = temp
+            if numeros[i] < numeros[i+1]: #Verifica se o número é menor do que o próximo da lista (Se não for não precisa mudar)
+                numeros[i+1],numeros[i] = numeros[i],numeros[i+1] #Inverte os números
+                print(numeros)
         except IndexError:
-            print("Lista finalizada")
-    if emordem == numeros: #Verifica se está ordenada. Se o ultimo passo deu o mesmo resultado 2 vezes signfica que está ordenada
-        print("Está ordenada")
-        print(f"A lista ordenada é {numeros}")
-        break
-    else:
-        print("Não está ordenada")
+            if emordem == numeros: #Verifica se está ordenada. Se o ultimo passo deu o mesmo resultado 2 vezes signfica que está ordenada
+                print(f"A lista ordenada é {numeros}")
+                ordenada = True
